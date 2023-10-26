d4811f84a1ee3a8aa18e7037077227836a20d821
StoopidBoi-6128 <149012620+StoopidBoi-6128@users.noreply.github.com>
2023-10-26 11:11:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
+# code by Ethan Calby and Karen Tong
+
+
 def encoder():
+    global encoded
     password = input("Please enter your password to encode: ")
     print("Your password has been encoded and stored!\n")
     encoded = ""
     for char in password:
         encoded += str(int(char) + 3)
+        print(password)
+        print(encoded)
     return encoded
 
-def decoder(password):
-    pass
+
+def decoder(encoded):
+    decoded = ""
+    for char in encoded:
+        decoded += str(int(char) - 3)
+    print("The encoded password is ", {encoded}, ", and the original password is ", {decoded}, ".")
+    return decoded
+
 
 def main():
+    global password
     while True:
-        print ("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
+        print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
         menu = int(input("Please enter an option: "))
         if menu == 1:
             password = encoder()
         elif menu == 2:
-            decoder(password)
+            decoder(encoded)
         elif menu == 3:
             break
+
 
 if __name__ == "__main__":
     main()
