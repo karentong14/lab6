733756ee0abc537385d08ab83f2fc9a056f72e8c
karentong14 <karentong@ufl.edu>
2023-10-26 00:35:32 -0400

    added main and encoder function

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello world")
+def encoder():
+    password = input("Please enter your password to encode: ")
+    print("Your password has been encoded and stored!\n")
+    encoded = ""
+    for char in password:
+        encoded += str(int(char) + 3)
+    return encoded
+
+def decoder(password):
+    pass
+
+def main():
+    while True:
+        print ("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
+        menu = int(input("Please enter an option: "))
+        if menu == 1:
+            password = encoder()
+        elif menu == 2:
+            decoder(password)
+        elif menu == 3:
+            break
+
+if __name__ == "__main__":
+    main()
